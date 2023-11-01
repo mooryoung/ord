e2731d324299551abe63c76d6c7d1ce7e2951b78
mooryoung <90918303+mooryoung@users.noreply.github.com>
2023-11-01 13:05:39 +0800

    Update media.rs
    support .text file

--- src/media.rs
+++ src/media.rs
@@ -51,6 +51,7 @@ impl Media {
     ("text/markdown;charset=utf-8", Media::Markdown, &["md"]),
     ("text/plain", Media::Text, &[]),
     ("text/plain;charset=utf-8", Media::Text, &["txt"]),
+    ("text/plain", Media::Text, &["text"]),
     ("video/mp4", Media::Video, &["mp4"]),
     ("video/webm", Media::Video, &["webm"]),
   ];
